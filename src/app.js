67c4d5c700b4c6506d47e2466f71606c3cd66041
JayPanchal-15092005 <trywebsiteapp56@gmail.com>
2025-03-09 17:01:11 +0530

    feat: implement comment functionality with CRUD operations and integrate morgan for logging

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import morgan from "morgan";
 
 const app = express();
 
@@ -13,12 +14,15 @@ app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({extended: true, limit: "20kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+app.use(morgan("dev"))
 
 // Routes import 
 
 import userRouter from "./routes/user.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/comment", commentRouter)
 
 export { 
     app,
---
+++ src/routes/comment.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+import { upload } from "../middlewares/multer.middlewares.js"
+import {
+    getVideoComment,
+    addComment,
+    updateComment,
+    deleteComment, 
+} from "../controllers/comment.controller.js"
+
+const router = Router()
+
+router.use(verifyJWT, upload.none());
+
+router.route("/:videoId").get(getVideoComment).post(addComment);
+router.route("/c/:commentId").delete(deleteComment).patch(updateComment)
+
+export default router
